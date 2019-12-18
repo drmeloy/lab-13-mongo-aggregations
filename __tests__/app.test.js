2f068754aff8a2e83fc69e5edb9c74f7d9b0099e
drmeloy <drmeloy@gmail.com>
2019-12-18 15:07:55 -0700

    began writing routes, first test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,8 +4,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Incident = require('../lib/models/Incident');
 
-describe('app routes', () => {
+describe('incidents routes', () => {
   beforeAll(() => {
     connect();
   });
@@ -14,7 +15,77 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  let incidentDate;
+  let incident;
+  beforeEach(async() => {
+    incidentDate = new Date();
+    incident = await Incident.create({
+      date: incidentDate,
+      state: 'Idaho',
+      cityOrCounty: 'Ada',
+      address: '456 E Blah Blah Ln',
+      numberKilled: 0,
+      numberInjured: 0,
+      gunType: 'Rocket launcher',
+      incidentCharacteristics: '\'Twas crazy!',
+      locationDescription: 'Jim-Bob\'s farmhouse',
+      numberOfGunsInvolved: 1,
+      notes: 'Nobody knows where Jim-Bob got a rocket launcher. Everyone just glad nobody got hurt.',
+      participantAge: '75',
+      participantGender: 'Male',
+      participantRelationship: 'Owner',
+      participantStatus: 'Uninjured',
+      participantType: 'Self-victim'
+    });
+  });
+
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('can create a new incident', () => {
+    const incidentDate = new Date();
+    return request(app)
+      .post('/api/v1/incidents')
+      .send({
+        date: incidentDate,
+        state: 'Idaho',
+        cityOrCounty: 'Ada',
+        address: '456 E Blah Blah Ln',
+        numberKilled: 0,
+        numberInjured: 0,
+        gunType: 'Rocket launcher',
+        incidentCharacteristics: '\'Twas crazy!',
+        locationDescription: 'Jim-Bob\'s farmhouse',
+        numberOfGunsInvolved: 1,
+        notes: 'Nobody knows where Jim-Bob got a rocket launcher. Everyone just glad nobody got hurt.',
+        participantAge: '75',
+        participantGender: 'Male',
+        participantRelationship: 'Owner',
+        participantStatus: 'Uninjured',
+        participantType: 'Self-victim'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          date: incidentDate.toISOString(),
+          state: 'Idaho',
+          cityOrCounty: 'Ada',
+          address: '456 E Blah Blah Ln',
+          numberKilled: 0,
+          numberInjured: 0,
+          gunType: 'Rocket launcher',
+          incidentCharacteristics: '\'Twas crazy!',
+          locationDescription: 'Jim-Bob\'s farmhouse',
+          numberOfGunsInvolved: 1,
+          notes: 'Nobody knows where Jim-Bob got a rocket launcher. Everyone just glad nobody got hurt.',
+          participantAge: '75',
+          participantGender: 'Male',
+          participantRelationship: 'Owner',
+          participantStatus: 'Uninjured',
+          participantType: 'Self-victim',
+          __v: 0
+        });
+      });
   });
 });
